1185a549923c4bad8c7965d94bde3aa7f6088b60
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-10 09:12:57 +0300

    Add project files.

---
+++ Overload/Program.cs
@@ -0,0 +1,10 @@
+﻿using Overload;
+
+int[] sayilar = { 10, 20, 30, 100 };
+Console.WriteLine(Hesaplama.Topla(sayilar));
+
+Math.Sin(Math.PI/6);
+Math.Sqrt(Math.PI);
+
+Console.WriteLine(Math.Sin(Math.PI / 6));
+Console.WriteLine(Math.Sqrt(Math.PI));
